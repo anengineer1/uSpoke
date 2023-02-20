45e19387d28fc79429b9971b8e3e8ab35aa93a07
yamir <yamir@mandragora.mandragora>
2023-02-20 16:47:32 +0100

    Making table contents NOT NULL

--- database/create_table.sql
+++ database/create_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE messages(
-       id serial PRIMARY KEY,
-       message_datetime TIMESTAMP with time zone,
-       username VARCHAR(255),
-       message TEXT
+       id serial PRIMARY KEY NOT NULL,
+       created_at_utc TIMESTAMP with time zone NOT NULL,
+       username VARCHAR(255) NOT NULL,
+       message TEXT NOT NULL
 );
